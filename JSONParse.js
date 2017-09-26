db323dc8cce896a3713b473b198b6625937a3063
Neethu Mohandas <prayaganeethu@gmail.com>
2017-09-26 16:08:53 +0530

    parses Boolean and null values

--- JSONParse.js
+++ JSONParse.js
@@ -1,5 +1,6 @@
+
 exports.parseJSON = function(JSONInput) {
-	return parseNull(JSONInput);
+	return parseFalse(JSONInput);
 }
 
 function parseNull(JSONInput) {
@@ -8,7 +9,26 @@ function parseNull(JSONInput) {
 			if(JSONInput.slice(i, i+4) == 'null')	{
 				JSONInput = JSONInput.slice(i+4,JSONInput.length);
 				return [null,JSONInput];
-			}		
+			}
+	return ["", JSONInput];		
 }
 
+function parseTrue(JSONInput) {
+	for(let i = 0; i < JSONInput.length - 1; i++) 
+		if(JSONInput[i] == 't') 
+			if(JSONInput.slice(i, i+4) == 'true')	{
+				JSONInput = JSONInput.slice(i+4,JSONInput.length);
+				return [true,JSONInput];
+			}
+	return ["", JSONInput];		
+}
 
+function parseFalse(JSONInput) {
+	for(let i = 0; i < JSONInput.length - 1; i++) 
+		if(JSONInput[i] == 'f') 
+			if(JSONInput.slice(i, i+5) == 'false')	{
+				JSONInput = JSONInput.slice(i+5,JSONInput.length);
+				return [false,JSONInput];
+			}
+	return ["", JSONInput];		
+}
